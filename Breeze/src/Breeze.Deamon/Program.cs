157e7f0d3c247a0583212dc760310b3b0f2c6022
Jeremy Bokobza <bokobza@gmail.com>
2017-03-27 10:42:08 +0100

    Added Deamon project that will be reponsible for loading the full node and the other dependencies

---
+++ Breeze/src/Breeze.Deamon/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using Microsoft.Extensions.Logging;
+using Stratis.Bitcoin;
+using Stratis.Bitcoin.Builder;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Logging;
+
+namespace Breeze.Deamon
+{
+	public class Program
+    {
+        public static void Main(string[] args)
+        {
+			// configure Full Node
+			Logs.Configure(new LoggerFactory().AddConsole(LogLevel.Trace, false));
+			NodeSettings nodeSettings = NodeSettings.FromArguments(args);
+
+			var node = (FullNode)new FullNodeBuilder()
+				.UseNodeSettings(nodeSettings)	
+				//.UseWallet()
+				//.UseApi()
+				//.UseBlockNotification()			
+				.Build();
+
+			System.Console.WriteLine();
+
+			// == shut down thread ==
+			new Thread(() =>
+			{
+
+				System.Console.WriteLine("Press one key to stop");
+				System.Console.ReadLine();
+				node.Dispose();
+			})
+			{
+				IsBackground = true //so the process terminates
+			}.Start();
+
+			// start Full Node - this will also start the API
+			node.Start();
+			node.WaitDisposed();
+			node.Dispose();
+			
+		}
+    }
+}
