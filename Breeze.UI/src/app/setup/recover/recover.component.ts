d163c9a2f87dbd9f6ac9d06399d0bae7d39ba642
dev0tion <dev0tion@hotmail.com>
2017-04-10 14:04:10 +0200

    Add recover component

---
+++ Breeze.UI/src/app/setup/recover/recover.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../shared/api/api.service'
+import { WalletRecovery } from '../../shared/wallet-recovery'
+
+@Component({
+  selector: 'app-recover',
+  templateUrl: './recover.component.html',
+  styleUrls: ['./recover.component.css']
+})
+export class RecoverComponent implements OnInit {
+
+  constructor(private apiService: ApiService) { }
+  private walletRecovery: WalletRecovery;
+  private responseBody: string;
+
+  ngOnInit() {
+  }
+
+  private recoverWallet(mnemonic: string, password: string, creationDate: string, network: string) {
+    this.walletRecovery.mnemonic = mnemonic;
+    this.walletRecovery.password = password;
+    this.walletRecovery.date = creationDate;
+    this.walletRecovery.network = network;
+
+    this.apiService
+      .recoverWallet(this.walletRecovery)
+      .subscribe((response: string) => this.responseBody = response,
+      () => console.log("recoverWallet() completed"));
+  }
+}
