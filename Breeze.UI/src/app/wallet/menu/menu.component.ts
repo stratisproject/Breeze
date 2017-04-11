dd6bd0d1a10ef2f62f61895ade3d66bd1187021d
dev0tion <dev0tion@hotmail.com>
2017-04-11 16:53:45 +0200

    Add logout

--- Breeze.UI/src/app/wallet/menu/menu.component.ts
+++ Breeze.UI/src/app/wallet/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent {
+  constructor(private router: Router) {}
 
+  private logOut() {
+    this.router.navigate(['/login']);
+  }
 }
