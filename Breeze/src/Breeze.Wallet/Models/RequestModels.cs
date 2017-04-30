d8bcbeb88edee52b4f4d0271f2b359b61e0c5e26
Jeremy Bokobza <bokobza@gmail.com>
2017-04-30 15:20:38 +0100

    Fixed formatting

--- Breeze/src/Breeze.Wallet/Models/RequestModels.cs
+++ Breeze/src/Breeze.Wallet/Models/RequestModels.cs
@@ -4,48 +4,48 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Breeze.Wallet.Models
 {
-	/// <summary>
-	/// Object used to create a new wallet
-	/// </summary>
-	public class WalletCreationRequest
-	{
-		[Required(ErrorMessage = "A password is required.")]
-		public string Password { get; set; }
+    /// <summary>
+    /// Object used to create a new wallet
+    /// </summary>
+    public class WalletCreationRequest
+    {
+        [Required(ErrorMessage = "A password is required.")]
+        public string Password { get; set; }
 
-		public string Network { get; set; }
+        public string Network { get; set; }
 
-		public string FolderPath { get; set; }
+        public string FolderPath { get; set; }
 
-		[Required(ErrorMessage = "The name of the wallet to create is missing.")]
-		public string Name { get; set; }
-	}
+        [Required(ErrorMessage = "The name of the wallet to create is missing.")]
+        public string Name { get; set; }
+    }
 
-	public class WalletLoadRequest
-	{
-		[Required(ErrorMessage = "A password is required.")]
-		public string Password { get; set; }
+    public class WalletLoadRequest
+    {
+        [Required(ErrorMessage = "A password is required.")]
+        public string Password { get; set; }
 
-		public string FolderPath { get; set; }
+        public string FolderPath { get; set; }
 
-		[Required(ErrorMessage = "The name of the wallet is missing.")]
-		public string Name { get; set; }
-	}
+        [Required(ErrorMessage = "The name of the wallet is missing.")]
+        public string Name { get; set; }
+    }
 
-	public class WalletRecoveryRequest
-	{
-		[Required(ErrorMessage = "A mnemonic is required.")]
-		public string Mnemonic { get; set; }
+    public class WalletRecoveryRequest
+    {
+        [Required(ErrorMessage = "A mnemonic is required.")]
+        public string Mnemonic { get; set; }
 
-		[Required(ErrorMessage = "A password is required.")]
-		public string Password { get; set; }
+        [Required(ErrorMessage = "A password is required.")]
+        public string Password { get; set; }
 
-		public string FolderPath { get; set; }
+        public string FolderPath { get; set; }
 
-		[Required(ErrorMessage = "The name of the wallet is missing.")]
-		public string Name { get; set; }
+        [Required(ErrorMessage = "The name of the wallet is missing.")]
+        public string Name { get; set; }
 
-		public string Network { get; set; }
-	}
+        public string Network { get; set; }
+    }
 
     public class WalletHistoryRequest
     {
@@ -57,32 +57,32 @@ namespace Breeze.Wallet.Models
     }
 
     public class WalletName
-	{
-		[Required(ErrorMessage = "The name of the wallet is missing.")]
-		public string Name { get; set; }
-	}
+    {
+        [Required(ErrorMessage = "The name of the wallet is missing.")]
+        public string Name { get; set; }
+    }
 
-	public class BuildTransactionRequest
-	{
-		[Required(ErrorMessage = "A password is required.")]
-		public string Password { get; set; }
+    public class BuildTransactionRequest
+    {
+        [Required(ErrorMessage = "A password is required.")]
+        public string Password { get; set; }
 
-		[Required(ErrorMessage = "A destination address is required.")]
-		public string Address { get; set; }
+        [Required(ErrorMessage = "A destination address is required.")]
+        public string Address { get; set; }
 
-		[Required(ErrorMessage = "An amount is required.")]
-		public string Amount { get; set; }
+        [Required(ErrorMessage = "An amount is required.")]
+        public string Amount { get; set; }
 
-		[Required(ErrorMessage = "A fee type is required. It can be 'low', 'medium' or 'high'.")]
-		public string FeeType { get; set; }
-		
-		public bool AllowUnconfirmed { get; set; }
-	}
+        [Required(ErrorMessage = "A fee type is required. It can be 'low', 'medium' or 'high'.")]
+        public string FeeType { get; set; }
 
-	public class SendTransactionRequest
-	{
-		[Required(ErrorMessage = "A transaction in hexadecimal format is required.")]
-		public string Hex { get; set; }
-	}
+        public bool AllowUnconfirmed { get; set; }
+    }
+
+    public class SendTransactionRequest
+    {
+        [Required(ErrorMessage = "A transaction in hexadecimal format is required.")]
+        public string Hex { get; set; }
+    }
 
 }
--- Breeze/src/Breeze.Wallet/Models/WalletHistoryModel.cs
+++ Breeze/src/Breeze.Wallet/Models/WalletHistoryModel.cs
@@ -9,30 +9,30 @@ namespace Breeze.Wallet.Models
 {
     public class WalletHistoryModel
     {
-		[JsonProperty(PropertyName = "transactions")]
-		public List<TransactionItem> Transactions { get; set; }
-	}
+        [JsonProperty(PropertyName = "transactions")]
+        public List<TransactionItem> Transactions { get; set; }
+    }
 
-	public class TransactionItem
-	{
-	    /// <summary>
-	    /// The Base58 representation of this address.
-	    /// </summary>
-	    [JsonProperty(PropertyName = "address")]
-	    public string Address { get; set; }
+    public class TransactionItem
+    {
+        /// <summary>
+        /// The Base58 representation of this address.
+        /// </summary>
+        [JsonProperty(PropertyName = "address")]
+        public string Address { get; set; }
 
         [JsonProperty(PropertyName = "txId")]
-		[JsonConverter(typeof(UInt256JsonConverter))]
+        [JsonConverter(typeof(UInt256JsonConverter))]
         public uint256 TransactionId { get; set; }
 
-		[JsonProperty(PropertyName = "amount")]
-		public Money Amount { get; set; }
+        [JsonProperty(PropertyName = "amount")]
+        public Money Amount { get; set; }
 
-		[JsonProperty(PropertyName = "confirmed")]
-		public bool Confirmed { get; set; } 
+        [JsonProperty(PropertyName = "confirmed")]
+        public bool Confirmed { get; set; }
 
-		[JsonProperty(PropertyName = "timestamp")]
-		[JsonConverter(typeof(DateTimeOffsetConverter))]
-        public DateTimeOffset Timestamp { get; set; }        
+        [JsonProperty(PropertyName = "timestamp")]
+        [JsonConverter(typeof(DateTimeOffsetConverter))]
+        public DateTimeOffset Timestamp { get; set; }
     }
 }
