a464e3f159af52e124ad56b2d49f10a244fc6b67
Jeremy Bokobza <bokobza@gmail.com>
2017-05-30 17:33:46 +0100

    Added api methods

--- Breeze/src/Breeze.TumbleBit.Client/TumblerService.cs
+++ Breeze/src/Breeze.TumbleBit.Client/TumblerService.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Breeze.TumbleBit.Models;
 using Flurl;
 using Flurl.Http;
 using Flurl.Http.Configuration;
+using NBitcoin;
 using NBitcoin.JsonConverters;
 using Newtonsoft.Json;
+using NTumbleBit;
 using NTumbleBit.ClassicTumbler;
 using NTumbleBit.JsonConverters;
+using NTumbleBit.PuzzlePromise;
+using NTumbleBit.PuzzleSolver;
+using PuzzlePromise = NTumbleBit.PuzzlePromise;
+using PuzzleSolver = NTumbleBit.PuzzleSolver;
 
 namespace Breeze.TumbleBit.Client
 {
@@ -31,6 +38,76 @@ namespace Breeze.TumbleBit.Client
         public async Task<ClassicTumblerParameters> GetClassicTumblerParametersAsync()
         {
             ClassicTumblerParameters result = await this.serverAddress.AppendPathSegment("/api/v1/tumblers/0/parameters").GetJsonAsync<ClassicTumblerParameters>();
+            return result;
+        }
+
+        /// <inheritdoc />
+        public async Task<UnsignedVoucherInformation> AskUnsignedVoucher()
+        {
+            UnsignedVoucherInformation result = await this.serverAddress.AppendPathSegment("api/v1/tumblers/0/vouchers/").GetJsonAsync<UnsignedVoucherInformation>();
+            return result;
+        }
+
+        /// <inheritdoc />
+        public async Task<PuzzleSolution> SignVoucherAsync(SignVoucherRequest request)
+        {
+            PuzzleSolution result = await this.serverAddress.AppendPathSegment("api/v1/tumblers/0/clientchannels/confirm").PostJsonAsync(request).ReceiveJson<PuzzleSolution>();
+            return result;
+        }
+
+        /// <inheritdoc />
+        public async Task<ScriptCoin> OpenChannelAsync(OpenChannelRequest request)
+        {
+            ScriptCoin result = await this.serverAddress.AppendPathSegment("api/v1/tumblers/0/channels/").PostJsonAsync(request).ReceiveJson<ScriptCoin>();
+            return result;
+        }
+
+        /// <inheritdoc />
+        public async Task<TumblerEscrowKeyResponse> RequestTumblerEscrowKeyAsync(int cycleStart)
+        {
+            TumblerEscrowKeyResponse result = await this.serverAddress.AppendPathSegment("api/v1/tumblers/0/clientchannels/").PostJsonAsync(cycleStart).ReceiveJson<TumblerEscrowKeyResponse>();
+            return result;
+        }
+
+        /// <inheritdoc />
+        public async Task<ServerCommitmentsProof> CheckRevelationAsync(int cycleId, string channelId, PuzzlePromise.ClientRevelation revelation)
+        {
+            ServerCommitmentsProof result = await this.serverAddress.AppendPathSegment($"api/v1/tumblers/0/channels/{cycleId}/{channelId}/checkrevelation").PostJsonAsync(revelation).ReceiveJson<ServerCommitmentsProof>();
+            return result;
+        }
+
+        /// <inheritdoc />
+        public async Task<SolutionKey[]> CheckRevelationAsync(int cycleId, string channelId, PuzzleSolver.ClientRevelation revelation)
+        {
+            SolutionKey[] result = await this.serverAddress.AppendPathSegment($"api/v1/tumblers/0/clientschannels/{cycleId}/{channelId}/checkrevelation").PostJsonAsync(revelation).ReceiveJson<SolutionKey[]>();
+            return result;
+        }
+
+        /// <inheritdoc />
+        public async Task<PuzzlePromise.ServerCommitment[]> SignHashesAsync(int cycleId, string channelId, SignaturesRequest sigReq)
+        {
+            PuzzlePromise.ServerCommitment[] result = await this.serverAddress.AppendPathSegment($"api/v1/tumblers/0/channels/{cycleId}/{channelId}/signhashes").PostJsonAsync(sigReq).ReceiveJson<PuzzlePromise.ServerCommitment[]>();
+            return result;
+        }
+
+        /// <inheritdoc />
+        public async Task<OfferInformation> CheckBlindFactorsAsync(int cycleId, string channelId, BlindFactor[] blindFactors)
+        {
+            OfferInformation result = await this.serverAddress.AppendPathSegment($"api/v1/tumblers/0/clientschannels/{cycleId}/{channelId}/checkblindfactors").PostJsonAsync(blindFactors).ReceiveJson<OfferInformation>();
+            return result;
+        }
+
+        /// <inheritdoc />
+        public async Task<PuzzleSolver.ServerCommitment[]> SolvePuzzlesAsync(int cycleId, string channelId, PuzzleValue[] puzzles)
+        {
+            PuzzleSolver.ServerCommitment[] result = await this.serverAddress.AppendPathSegment($"api/v1/tumblers/0/clientchannels/{cycleId}/{channelId}/solvepuzzles").PostJsonAsync(puzzles).ReceiveJson<PuzzleSolver.ServerCommitment[]>();
+            return result;
+        }
+
+        /// <inheritdoc />
+        public async Task<SolutionKey[]> FulfillOfferAsync(int cycleId, string channelId, TransactionSignature clientSignature)
+        {
+            SolutionKey[] result = await this.serverAddress.AppendPathSegment($"api/v1/tumblers/0/clientchannels/{cycleId}/{channelId}/offer").PostJsonAsync(clientSignature).ReceiveJson<SolutionKey[]>();
             return result;
         }
     }
