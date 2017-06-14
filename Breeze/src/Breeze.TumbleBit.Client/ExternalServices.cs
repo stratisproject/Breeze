25670725e2af92fec12c1d3600d34b18db73ef5e
Jeremy Bokobza <bokobza@gmail.com>
2017-06-14 14:04:47 +0100

    Added feerate for the tumbler client transactions

--- Breeze/src/Breeze.TumbleBit.Client/ExternalServices.cs
+++ Breeze/src/Breeze.TumbleBit.Client/ExternalServices.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using NBitcoin;
 using NTumbleBit;
+using Stratis.Bitcoin.MemoryPool;
 
 namespace Breeze.TumbleBit.Client
 {
@@ -33,7 +34,20 @@ namespace Breeze.TumbleBit.Client
 
         public FeeRate GetFeeRate()
         {
-            return null;
+            decimal relayFee = MempoolValidator.MinRelayTxFee.FeePerK.ToUnit(MoneyUnit.BTC);
+            var minimumRate = new FeeRate(Money.Coins(relayFee * 2), 1000); //0.00002000 BTC/kB
+            var fallbackFeeRate = new FeeRate(Money.Satoshis(50), 1);       //0.00050000 BTC/kB
+
+            // TODO add real fee estimation 
+            //var rate = _RPCClient.TryEstimateFeeRate(1) ??
+            //           _RPCClient.TryEstimateFeeRate(2) ??
+            //           _RPCClient.TryEstimateFeeRate(3) ??
+            //           FallBackFeeRate;
+
+            //if (rate < MinimumFeeRate)
+            //    rate = MinimumFeeRa
+            
+            return fallbackFeeRate;
         }
     }
 }
