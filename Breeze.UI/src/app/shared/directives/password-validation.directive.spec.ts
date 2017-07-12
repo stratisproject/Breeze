a41c68bd8b956f4c177283d220943db4ca6d229e
dev0tion <dev0tion@hotmail.com>
2017-07-12 17:33:56 +0200

    Add password confirmation box, add password strength checker

---
+++ Breeze.UI/src/app/shared/directives/password-validation.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PasswordValidationDirective } from './password-validation.directive';
+
+describe('PasswordValidationDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PasswordValidationDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- Breeze.UI/src/app/shared/shared.module.ts
+++ Breeze.UI/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoinNotationPipe } from './pipes/coin-notation.pipe';
 import { AutoFocusDirective } from './directives/auto-focus.directive';
+import { PasswordValidationDirective } from './directives/password-validation.directive';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [CoinNotationPipe, AutoFocusDirective],
+  declarations: [CoinNotationPipe, AutoFocusDirective, PasswordValidationDirective],
   exports: [CoinNotationPipe, AutoFocusDirective]
 })
 
