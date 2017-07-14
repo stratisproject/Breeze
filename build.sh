6dd092d4a6225cf8effa97e91a8a63d2a9dd549a
Jeremy Bokobza <bokobza@gmail.com>
2017-07-14 12:11:31 +0100

    Added OS name and version to the output files for OSX releases

--- build.sh
+++ build.sh
@@ -3,15 +3,21 @@
 # exit if error
 set -o errexit
 
+# define a few variables
+app_output_name="breeze-$TRAVIS_OS_NAME.$os_version-$arch-$configuration"
+api_output_name="api-$TRAVIS_OS_NAME.$os_version-$arch-$configuration"
+
 echo "current environment variables:"
 echo "OS name:" $TRAVIS_OS_NAME
+echo "OS version:" $os_version
 echo "Platform:" $os_platform
 echo "Build directory:" $TRAVIS_BUILD_DIR
 echo "Node version:" $TRAVIS_NODE_VERSION
 echo "Architecture:" $arch
 echo "Configuration:" $configuration
-echo "App output name" $app_output_name
-echo "Api output name" $api_output_name
+echo "App output name:" $app_output_name
+echo "Api output name:" $api_output_name
+
 
 dotnet --info
 
