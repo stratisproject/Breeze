4b931a5b8b194382c80cf382cb5c521ef5559e48
Daniel Gould <dcsled@gmail.com>
2017-08-04 15:22:20 -0400

    add form & control based on sendForm

--- Breeze.UI/src/app/wallet/tumblebit/tumblebit.component.ts
+++ Breeze.UI/src/app/wallet/tumblebit/tumblebit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 import { ApiService } from '../../shared/services/api.service';
 import { GlobalService } from '../../shared/services/global.service';
@@ -15,10 +16,57 @@ import { Subscription } from 'rxjs/Subscription';
 })
 
 export class TumblebitComponent implements OnInit {
-  constructor(private apiService: ApiService, private globalService: GlobalService, private modalService: NgbModal) {}
+  constructor(private apiService: ApiService, private globalService: GlobalService, private modalService: NgbModal, private fb: FormBuilder) {
+    this.buildTumbleBitForm();
+  }
 
   private confirmedBalance: number;
   private walletBalanceSubscription: Subscription;
+
+  private tumblebitForm: FormGroup;
+
+  private buildTumbleBitForm(): void {
+    this.tumblebitForm = this.fb.group({
+      'destination': ['', Validators.required],
+    })
+
+    this.tumblebitForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+    
+    this.onValueChanged();
+  }
+
+  // TODO: abstract to a shared utility lib
+  onValueChanged(data?: any) {
+    console.log('changed')
+    if (!this.tumblebitForm) { return; }
+    const form = this.tumblebitForm;
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        console.log(control)
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  formErrors = {
+    'destination': '',
+  };
+
+  validationMessages = {
+    'destination': {
+      'required': 'A destination is required.'
+    }
+  }
+
+  private tumble() {
+    console.log(this.tumblebitForm);
+  }
 
   ngOnInit() {
     this.getWalletBalance();
@@ -36,7 +84,6 @@ export class TumblebitComponent implements OnInit {
           if (response.status >= 200 && response.status < 400) {
               let balanceResponse = response.json();
               this.confirmedBalance = balanceResponse.balances[0].amountConfirmed;
-              this.unconfirmedBalance = balanceResponse.balances[0].amountUnconfirmed;
           }
         },
         error => {
