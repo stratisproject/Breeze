e9cc5ff7caf9871b1b841046caf12ed49653c01a
dev0tion <dev0tion@hotmail.com>
2017-09-26 15:49:04 +0200

    Highlight current active coin

--- Breeze.UI/src/app/wallet/sidebar/sidebar.component.ts
+++ Breeze.UI/src/app/wallet/sidebar/sidebar.component.ts
@@ -13,18 +13,30 @@ import { GlobalService } from '../../shared/services/global.service';
 export class SidebarComponent implements OnInit {
 
   constructor(private globalService: GlobalService, private router: Router, private modalService: NgbModal) { }
+  private bitcoinActive: Boolean;
 
   ngOnInit() {
+    if (this.globalService.getCoinName() === "Bitcoin") {
+      this.bitcoinActive = true;
+    } else if (this.globalService.getCoinName() === "Stratis") {
+      this.bitcoinActive = false;
+    }
   }
 
   private loadBitcoinWallet() {
+    this.toggleClass();
     this.globalService.setCoinName("Bitcoin");
     this.router.navigate(['/wallet']);
   }
 
   private loadStratisWallet() {
+    this.toggleClass();
     this.globalService.setCoinName("Stratis");
     this.router.navigate(['/wallet/stratis-wallet']);
+  }
+
+  private toggleClass(){
+    this.bitcoinActive = !this.bitcoinActive;
   }
 
   private logOut() {
