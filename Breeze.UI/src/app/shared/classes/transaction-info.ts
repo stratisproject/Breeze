a80a81de369b7d3fe636c6f94aeaa61cd4546301
dev0tion <dev0tion@hotmail.com>
2017-09-28 18:03:12 +0200

    Remove to/from from transaction details

--- Breeze.UI/src/app/shared/classes/transaction-info.ts
+++ Breeze.UI/src/app/shared/classes/transaction-info.ts
@@ -1,10 +1,9 @@
 export class TransactionInfo {
 
-    constructor(transactionType: string, transactionId: string, transactionAmount: number, transactionAddress: any, transactionFee: number, transactionConfirmedInBlock: number, transactionTimestamp: number) {
+    constructor(transactionType: string, transactionId: string, transactionAmount: number, transactionFee: number, transactionConfirmedInBlock: number, transactionTimestamp: number) {
       this.transactionType = transactionType;
       this.transactionId = transactionId;
       this.transactionAmount = transactionAmount;
-      this.transactionAddress = transactionAddress;
       this.transactionFee = transactionFee;
       this.transactionConfirmedInBlock = transactionConfirmedInBlock;
       this.transactionTimestamp = transactionTimestamp;
@@ -13,7 +12,6 @@ export class TransactionInfo {
     public transactionType: string;
     public transactionId: string;
     public transactionAmount: number;
-    public transactionAddress: string;
     public transactionFee: number;
     public transactionConfirmedInBlock?: number;
     public transactionTimestamp: number;
