1ca4e7a0308b7f749ce76fa10a71f5ab5ebcf67a
dev0tion <dev0tion@hotmail.com>
2017-10-17 15:41:35 +0200

    Quit on close button OS X

--- Breeze.UI/main.ts
+++ Breeze.UI/main.ts
@@ -91,9 +91,10 @@ app.on('will-quit', function () {
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  // if (process.platform !== 'darwin') {
+  //   app.quit();
+  // }
+  app.quit();
 });
 
 app.on('activate', function () {
