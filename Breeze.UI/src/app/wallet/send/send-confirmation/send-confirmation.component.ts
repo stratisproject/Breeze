d550e128d0b5ced5212f9491050c2dd4f85ed9ab
dev0tion <dev0tion@hotmail.com>
2017-11-22 23:57:35 +0100

    Add transaction fee to send confirmation

--- Breeze.UI/src/app/wallet/send/send-confirmation/send-confirmation.component.ts
+++ Breeze.UI/src/app/wallet/send/send-confirmation/send-confirmation.component.ts
@@ -4,6 +4,8 @@ import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { GlobalService } from '../../../shared/services/global.service';
 
+import { CoinNotationPipe } from '../../../shared/pipes/coin-notation.pipe';
+
 @Component({
   selector: 'app-send-confirmation',
   templateUrl: './send-confirmation.component.html',
@@ -12,6 +14,7 @@ import { GlobalService } from '../../../shared/services/global.service';
 export class SendConfirmationComponent implements OnInit {
 
   @Input() transaction: any;
+  @Input() transactionFee: any;
   constructor(private globalService: GlobalService, public activeModal: NgbActiveModal) { }
 
   public showDetails: boolean = false;
@@ -19,6 +22,8 @@ export class SendConfirmationComponent implements OnInit {
 
   ngOnInit() {
     this.coinUnit = this.globalService.getCoinUnit();
+    this.transactionFee = new CoinNotationPipe(this.globalService).transform(this.transactionFee);
+    this.transaction.amount = +this.transaction.amount + +this.transactionFee;
   }
 
   toggleDetails() {
