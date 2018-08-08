4515575f9ad67458755129bcc34723315eded6df
bokobza <bokobza@gmail.com>
2018-08-08 17:14:31 +0100

    moved npm instakk

--- build.sh
+++ build.sh
@@ -41,8 +41,6 @@ git submodule update --init --recursive
 
 cd $TRAVIS_BUILD_DIR/Breeze.UI
 
-npm install -g npm@6.2.0
-npm install -g cross-env
 npm install -g npx
 echo $log_prefix FINISHED restoring dotnet and npm packages
 
