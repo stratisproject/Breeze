7a63ac43f445f4395019e17bd872c826335616cc
Paul Herbert <paul.herbert@stratisplatform.com>
2018-09-24 16:17:30 +0100

    Bumped version.

--- Breeze.UI/src/app/wallet/advanced/advanced.service.ts
+++ Breeze.UI/src/app/wallet/advanced/advanced.service.ts
@@ -24,27 +24,31 @@ export class AdvancedService {
     }
 
     public getExtPubKey(): Observable<string> {
-        const url = `${this.urlPrefix}extpubkey?WalletName=${this.walletName}&AccountName=${this.accountName}`;
+        const url = this.makeUrl(`extpubkey?WalletName=${this.walletName}&AccountName=${this.accountName}`);
         return this.httpClient.get(url).map(x => x.toString());
     }
 
     public generateAddresses(count: number): Observable<string[]> {
-        const url = `${this.urlPrefix}unusedaddresses?WalletName=${this.walletName}&AccountName=${this.accountName}&Count=${count}`;
+        const url = this.makeUrl(`unusedaddresses?WalletName=${this.walletName}&AccountName=${this.accountName}&Count=${count}`);
         return this.httpClient.get(url).map(x => this.processAddresses(x));
     }
 
     public resyncFromDate(date: Date): Observable<any> {
         date = new Date(date.getFullYear(), date.getMonth(), date.getDate()); //<- Strip any time values
-        const url = `${this.urlPrefix}syncfromdate`;
+        const url = this.makeUrl('syncfromdate');
         const data = JSON.stringify(new dateRequest(date));
         return this.httpClient.post(url, data, {headers: this.headers}).map((x: Response) => x);
     }
 
     private processAddresses(response: any): string[] {
-        let addresses = new Array<string>();
+        const addresses = new Array<string>();
         for (const address of response) {
             addresses.push(address);
         } 
         return addresses;
+    }
+
+    private makeUrl(urlPostfix: string): string {
+        return `${this.urlPrefix}${urlPostfix}`;
     }
 }
